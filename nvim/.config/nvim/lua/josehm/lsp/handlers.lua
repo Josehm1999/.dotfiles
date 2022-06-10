125509feecb826beecf06825af19d58087933ca8
josehm <josehm1999@gmail.com>
2022-06-09 19:46:14 -0500

    Telescope remaps added

--- nvim/.config/nvim/lua/josehm/lsp/handlers.lua
+++ nvim/.config/nvim/lua/josehm/lsp/handlers.lua
@@ -47,11 +47,11 @@ M.setup = function()
 end
 
 local function lsp_highlight_document(client)
-  local status_ok, illuminate = pcall(require, "illuminate")
-    if not status_ok then
-    return
-  end
-  illuminate.on_attach(client)
+	local status_ok, illuminate = pcall(require, "illuminate")
+	if not status_ok then
+		return
+	end
+	illuminate.on_attach(client)
 end
 
 local function lsp_keymaps(bufnr)
@@ -67,76 +67,74 @@ local function lsp_keymaps(bufnr)
 	-- vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>ca", "<cmd>lua vim.lsp.buf.code_action()<CR>", opts)
 	vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>ca", "<cmd>Telescope lsp_code_actions<CR>", opts)
 	vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>f", "<cmd>Telescope diagnostics<CR>", opts)
-	vim.api.nvim_buf_set_keymap(bufnr, "n", "dk", '<cmd>lua vim.diagnostic.goto_prev({ border = "rounded" })<CR>', opts)
 	vim.api.nvim_buf_set_keymap(
 		bufnr,
 		"n",
-		"gl",
-		'<cmd>lua vim.lsp.diagnostic.open_float()<CR>',
+		"<leader>gp",
+		'<cmd>lua vim.diagnostic.goto_prev({ border = "rounded" })<CR>',
 		opts
 	)
-	vim.api.nvim_buf_set_keymap(bufnr, "n", "dj", '<cmd>lua vim.diagnostic.goto_next({ border = "rounded" })<CR>', opts)
+	vim.api.nvim_buf_set_keymap(bufnr, "n", "gl", "<cmd>lua vim.lsp.diagnostic.open_float()<CR>", opts)
+	vim.api.nvim_buf_set_keymap(
+		bufnr,
+		"n",
+		"<leader>gn",
+		'<cmd>lua vim.diagnostic.goto_next({ border = "rounded" })<CR>',
+		opts
+	)
 	-- vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>q", "<cmd>lua vim.diagnostic.setloclist()<CR>", opts)
-  vim.cmd [[ command! Format execute 'lua vim.lsp.buf.format({ async = true })' ]]
+	vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>f", "<cmd>lua vim.lsp.buf.format({ async = true })<CR>", opts)
+	vim.cmd([[ command! Format execute 'lua vim.lsp.buf.formatting()' ]])
 end
-
-
-
 
 M.on_attach = function(client, bufnr)
-if
-		client.name == "tsserver"
-		or client.name == "html"
-		or client.name == "sumneko_lua"
-		or client.name == "jsonls"
-	then
-		M.resolved_capabilities.document_formatting = false
-  else 
-local status_ok, cmp_nvim_lsp = pcall(require, "cmp_nvim_lsp")
-if not status_ok then
-	return
+	-- if
+	-- 		client.name == "tsserver"
+	-- 		or client.name == "html"
+	-- 		or client.name == "sumneko_lua"
+	-- 		or client.name == "jsonls"
+	-- 	then
+	--		M.resolved_capabilities.document_formatting = false
+	-- else
+	local status_ok, cmp_nvim_lsp = pcall(require, "cmp_nvim_lsp")
+	if not status_ok then
+		return
+	end
+	M.capabilities.textDocument.completion.completionItem.snippetSupport = true
+	M.capabilities = cmp_nvim_lsp.update_capabilities(M.capabilities)
+	lsp_keymaps(bufnr)
+	lsp_highlight_document(client)
 end
-M.capabilities.textDocument.completion.completionItem.snippetSupport = true
-M.capabilities = cmp_nvim_lsp.update_capabilities(capabilities)	
-  end
-
-lsp_keymaps(bufnr)
-lsp_highlight_document(client)
-end
-
-
 
 function M.enable_format_on_save()
-  vim.cmd [[
+	vim.cmd([[
     augroup format_on_save
       autocmd! 
       autocmd BufWritePre * lua vim.lsp.buf.formatting()
     augroup end
-  ]]
-  vim.notify "Enabled format on save"
+  ]])
+	vim.notify("Enabled format on save")
 end
 
 function M.disable_format_on_save()
-  M.remove_augroup "format_on_save"
-  vim.notify "Disabled format on save"
+	M.remove_augroup("format_on_save")
+	vim.notify("Disabled format on save")
 end
 
 function M.toggle_format_on_save()
-  if vim.fn.exists "#format_on_save#BufWritePre" == 0 then
-    M.enable_format_on_save()
-  else
-    M.disable_format_on_save()
-  end
+	if vim.fn.exists("#format_on_save#BufWritePre") == 0 then
+		M.enable_format_on_save()
+	else
+		M.disable_format_on_save()
+	end
 end
 
 function M.remove_augroup(name)
-  if vim.fn.exists("#" .. name) == 1 then
-    vim.cmd("au! " .. name)
-  end
+	if vim.fn.exists("#" .. name) == 1 then
+		vim.cmd("au! " .. name)
+	end
 end
 
-vim.cmd [[ command! LspToggleAutoFormat execute 'lua require("josehm.lsp.handlers").toggle_format_on_save()' ]]
-
+vim.cmd([[ command! LspToggleAutoFormat execute 'lua require("josehm.lsp.handlers").toggle_format_on_save()' ]])
 
 return M
-
--- nvim/.config/nvim/lua/josehm/telescope.lua
+++ nvim/.config/nvim/lua/josehm/telescope.lua
@@ -1,154 +1,152 @@
 local status_ok, telescope = pcall(require, "telescope")
 if not status_ok then
-  return
+	return
 end
 
-local actions = require('telescope.actions')
-local action_state = require('telescope.actions.state')
-local action_layout = require('telescope.actions.layout')
+local actions = require("telescope.actions")
+local action_state = require("telescope.actions.state")
+local action_layout = require("telescope.actions.layout")
 
 local set_prompt_to_entry_value = function(prompt_bufnr)
 	local entry = action_state.get_selected_entry()
-  	if not entry or not type(entry) == "table" then
-    		return
+	if not entry or not type(entry) == "table" then
+		return
 	end
 
 	action_state.get_current_picker(prompt_bufnr):reset_prompt(entry.ordinal)
 end
 
+telescope.setup({
+	defaults = {
+		prompt_prefix = "❯ ",
+		selection_caret = "❯ ",
+		winblend = 0,
+		color_devicons = true,
+		file_ignore_patterns = { "node_modules", ".git" },
+		path_display = { "smart" },
+		find_files = { hidden = true },
 
-telescope.setup {
-    defaults = {
-	prompt_prefix = "❯ ",
-	selection_caret = "❯ ",
-	winblend = 0,
-	color_devicons = true,
-	file_ignore_patterns = {'node_modules','.git'},
-	path_display = { "smart" },
-	find_files = { hidden = true },
-	
-	layout_strategy = "horizontal",
-    	
-	layout_config = {
-      		width = 0.95,
-     		height = 0.85,
-      		-- preview_cutoff = 120,
-     		prompt_position = "top",
+		layout_strategy = "horizontal",
 
-      	horizontal = {
-        	preview_width = function(_, cols, _)
-          		if cols > 200 then
-            			return math.floor(cols * 0.4)
-          		else
-            			return math.floor(cols * 0.6)
-          		end
-        	end,
-      	},
+		layout_config = {
+			width = 0.95,
+			height = 0.85,
+			-- preview_cutoff = 120,
+			prompt_position = "top",
 
-      	vertical = {
-        	width = 0.9,
-        	height = 0.95,
-        	preview_height = 0.5,
-      	},
+			horizontal = {
+				preview_width = function(_, cols, _)
+					if cols > 200 then
+						return math.floor(cols * 0.4)
+					else
+						return math.floor(cols * 0.6)
+					end
+				end,
+			},
 
-      	flex = {
-        	horizontal = {
-          	preview_width = 0.9,
-        	},
-      	  },
+			vertical = {
+				width = 0.9,
+				height = 0.95,
+				preview_height = 0.5,
+			},
+
+			flex = {
+				horizontal = {
+					preview_width = 0.9,
+				},
+			},
+		},
+
+		selection_strategy = "reset",
+		sorting_strategy = "descending",
+		scroll_strategy = "cycle",
+
+		mappings = {
+			i = {
+				["<C-n>"] = actions.cycle_history_next,
+				["<C-p>"] = actions.cycle_history_prev,
+				["<C-j>"] = actions.move_selection_next,
+				["<C-k>"] = actions.move_selection_previous,
+				["<C-c>"] = actions.close,
+				["<Down>"] = actions.move_selection_next,
+				["<Up>"] = actions.move_selection_previous,
+				["<CR>"] = actions.select_default,
+				["<C-x>"] = actions.select_horizontal,
+				["<C-v>"] = actions.select_vertical,
+				["<C-t>"] = actions.select_tab,
+				["<C-u>"] = actions.preview_scrolling_up,
+				["<C-d>"] = actions.preview_scrolling_down,
+				["<PageUp>"] = actions.results_scrolling_up,
+				["<PageDown>"] = actions.results_scrolling_down,
+				["<Tab>"] = actions.toggle_selection + actions.move_selection_worse,
+				["<S-Tab>"] = actions.toggle_selection + actions.move_selection_better,
+				["<C-q>"] = actions.send_to_qflist + actions.open_qflist,
+				["<M-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
+				["<C-l>"] = actions.complete_tag,
+				["<C-_>"] = actions.which_key, -- keys from pressing <C-/>
+			},
+			n = {
+				["<esc>"] = actions.close,
+				["<CR>"] = actions.select_default,
+				["<C-x>"] = actions.select_horizontal,
+				["<C-v>"] = actions.select_vertical,
+				["<C-t>"] = actions.select_tab,
+
+				["<Tab>"] = actions.toggle_selection + actions.move_selection_worse,
+				["<S-Tab>"] = actions.toggle_selection + actions.move_selection_better,
+				["<C-q>"] = actions.send_to_qflist + actions.open_qflist,
+				["<M-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
+
+				["j"] = actions.move_selection_next,
+				["k"] = actions.move_selection_previous,
+				["h"] = actions.move_to_top,
+				["m"] = actions.move_to_middle,
+				["l"] = actions.move_to_bottom,
+
+				["<Down>"] = actions.move_selection_next,
+				["<Up>"] = actions.move_selection_previous,
+				["gg"] = actions.move_to_top,
+				["G"] = actions.move_to_bottom,
+
+				["<C-u>"] = actions.preview_scrolling_up,
+				["<C-d>"] = actions.preview_scrolling_down,
+
+				["<PageUp>"] = actions.results_scrolling_up,
+				["<PageDown>"] = actions.results_scrolling_down,
+				["?"] = actions.which_key,
+			},
+		},
+
+		file_previewer = require("telescope.previewers").vim_buffer_cat.new,
+		grep_previewer = require("telescope.previewers").vim_buffer_vimgrep.new,
+		qflist_previewer = require("telescope.previewers").vim_buffer_qflist.new,
+
+		pickers = {
+			fd = {
+				mappings = {
+					n = {
+						["kj"] = "close",
+					},
+				},
+			},
+		},
+
+		extensions = {
+			fzy_native = {
+				override_generic_sorter = false,
+				override_file_sorter = true,
+			},
+		},
+
+		["ui-select"] = {
+			require("telescope.themes").get_dropdown({
+				-- even more opts
+			}),
+		},
 	},
-
-	selection_strategy = "reset",
-    	sorting_strategy = "descending",
-    	scroll_strategy = "cycle",
-
-	
-        mappings = {
-            i = {
-        ["<C-n>"] = actions.cycle_history_next,
-        ["<C-p>"] = actions.cycle_history_prev,
-        ["<C-j>"] = actions.move_selection_next,
-        ["<C-k>"] = actions.move_selection_previous,
-        ["<C-c>"] = actions.close,
-        ["<Down>"] = actions.move_selection_next,
-        ["<Up>"] = actions.move_selection_previous,
-        ["<CR>"] = actions.select_default,
-        ["<C-x>"] = actions.select_horizontal,
-        ["<C-v>"] = actions.select_vertical,
-        ["<C-t>"] = actions.select_tab,
-        ["<C-u>"] = actions.preview_scrolling_up,
-        ["<C-d>"] = actions.preview_scrolling_down,
-        ["<PageUp>"] = actions.results_scrolling_up,
-        ["<PageDown>"] = actions.results_scrolling_down,
-        ["<Tab>"] = actions.toggle_selection + actions.move_selection_worse,
-        ["<S-Tab>"] = actions.toggle_selection + actions.move_selection_better,
-        ["<C-q>"] = actions.send_to_qflist + actions.open_qflist,
-        ["<M-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
-        ["<C-l>"] = actions.complete_tag,
-        ["<C-_>"] = actions.which_key, -- keys from pressing <C-/>
-	},
-	n = {
-      ["<esc>"] = actions.close,
-      ["<CR>"] = actions.select_default,
-      ["<C-x>"] = actions.select_horizontal,
-      ["<C-v>"] = actions.select_vertical,
-      ["<C-t>"] = actions.select_tab,
-
-      ["<Tab>"] = actions.toggle_selection + actions.move_selection_worse,
-      ["<S-Tab>"] = actions.toggle_selection + actions.move_selection_better,
-      ["<C-q>"] = actions.send_to_qflist + actions.open_qflist,
-      ["<M-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
-
-      ["j"] = actions.move_selection_next,
-      ["k"] = actions.move_selection_previous,
-      ["h"] = actions.move_to_top,
-      ["m"] = actions.move_to_middle,
-      ["l"] = actions.move_to_bottom,
-
-      ["<Down>"] = actions.move_selection_next,
-      ["<Up>"] = actions.move_selection_previous,
-      ["gg"] = actions.move_to_top,
-      ["G"] = actions.move_to_bottom,
-
-      ["<C-u>"] = actions.preview_scrolling_up,
-      ["<C-d>"] = actions.preview_scrolling_down,
-
-      ["<PageUp>"] = actions.results_scrolling_up,
-      ["<PageDown>"] = actions.results_scrolling_down,
-      ["?"] = actions.which_key,
-    },
-  },
-
-    file_previewer = require("telescope.previewers").vim_buffer_cat.new,
-    grep_previewer = require("telescope.previewers").vim_buffer_vimgrep.new,
-    qflist_previewer = require("telescope.previewers").vim_buffer_qflist.new,
-
-    pickers = {
-     fd = {
-      mappings = {
-        n = {
-          ["kj"] = "close",
-        },
-      },
-     },
-    },
-
-    extensions = {
-	fzy_native = {
-              override_generic_sorter = false,
-              override_file_sorter = true,
-	},
-    },
-
-    ["ui-select"] = {
-      require("telescope.themes").get_dropdown {
-        -- even more opts
-      },
-    },
-  },
-}
-  _ = require("telescope").load_extension("fzy_native")
-  _ = require("telescope").load_extension("file_browser")
+})
+_ = require("telescope").load_extension("fzy_native")
+_ = require("telescope").load_extension("file_browser")
 
 local M = {}
 
@@ -171,10 +169,9 @@ function M.search_dotfiles()
 	})
 end
 
-function M.curr_buff ()
+function M.curr_buff()
 	local opt = require("telescope.themes").get_ivy({ height = 10, winblend = 10 })
 	require("telescope.builtin").current_buffer_fuzzy_find(opt)
 end
-
 
 return M
